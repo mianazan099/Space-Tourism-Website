9943d63d0fa0ffa8aacc0585923555e4e016e117
mianazan099 <mianazan099@gmail.com>
2022-08-10 22:57:31 +0500

    crew Js Done

--- js/crew.js
+++ js/crew.js
@@ -1,42 +1,33 @@
 const tabBtnList = document.querySelectorAll(".tab-btn");
-const destinationImg = document.querySelector(".destination-img img");
-const destinationName = document.querySelector(".destination-name");
-const destinationInfo = document.querySelector(".destination-info");
-const distance = document.querySelector(".distance");
-const travel = document.querySelector(".travel");
+const memberRole = document.querySelector(".member-role");
+const memberName = document.querySelector(".member-name");
+const memberInfo = document.querySelector(".member-info");
+const memberImg = document.querySelector(".member-img");
 
-const destinations = [
+const crew = [
   {
-    name: "Moon",
-    image: "./assets/destination/image-moon.webp",
-    description:
-      "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
-    distance: "384,400 km",
-    travel: "3 days",
+    name: "Douglas Hurley",
+    image: "./assets/crew/image-douglas-hurley.webp",
+    role: "Commander",
+    bio: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
   },
   {
-    name: "Mars",
-    image: "./assets/destination/image-mars.webp",
-    description:
-      "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
-    distance: "225 mil. km",
-    travel: "9 months",
+    name: "Mark Shuttleworth",
+    image: "./assets/crew/image-mark-shuttleworth.webp",
+    role: "Mission Specialist",
+    bio: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
   },
   {
-    name: "Europa",
-    image: "./assets/destination/image-europa.webp",
-    description:
-      "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
-    distance: "628 mil. km",
-    travel: "3 years",
+    name: "Victor Glover",
+    image: "./assets/crew/image-victor-glover.webp",
+    role: "Pilot",
+    bio: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
   },
   {
-    name: "Titan",
-    image: "./assets/destination/image-titan.webp",
-    description:
-      "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
-    distance: "1.6 bil. km",
-    travel: "7 years",
+    name: "Anousheh Ansari",
+    image: "./assets/crew/image-anousheh-ansari.webp",
+    role: "Flight Engineer",
+    bio: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
   },
 ];
 
@@ -46,11 +37,10 @@ tabBtnList.forEach((tab) => {
       t.classList.remove("tab-btn-active");
     });
     tab.classList.add("tab-btn-active");
-    const data = destinations[tab.getAttribute("data-number")];
-    destinationImg.src = data.image;
-    destinationName.textContent = data.name;
-    destinationInfo.textContent = data.description;
-    distance.textContent = data.distance;
-    travel.textContent = data.travel;
+    const data = crew[tab.getAttribute("data-number")];
+    memberRole.textContent = data.role;
+    memberName.textContent = data.name;
+    memberInfo.textContent = data.bio;
+    memberImg.style.backgroundImage = `url('${data.image}')`;
   });
 });
